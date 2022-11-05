3cccc40e14cbb1d150aed27edf5384c6a43aeb57
William Robersone <wmrobersone@yahoo.com>
2022-11-05 11:44:17 -0500

    first commit

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+          voluptatem? Quidem impedit repellendus quibusdam soluta repudiandae
+          ullam recusandae, commodi sunt consequatur quo dolore aut. Neque in
+          deserunt numquam eligendi voluptatibus.
+        </p>
+        <button type="button" className="custom__button">
+          Learn More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat,
+          voluptatem? Quidem impedit repellendus quibusdam soluta repudiandae
+          ullam recusandae, commodi sunt consequatur quo dolore aut. Neque in
+          deserunt numquam eligendi voluptatibus.
+        </p>
+        <button type="button" className="custom__button">
+          Learn More
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
